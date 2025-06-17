dad973552204d80a2dd955375706d644281c13a0
Neve-Is-Cool <145308823+Neve-Is-Cool@users.noreply.github.com>
2025-06-17 15:19:12 +1200

    Read checkbox added
    Next step is to induvidualise these as when one checkbox is ticked, all become ticked.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script>
   import Header from "../lib/Header.svelte"
   let books = []
+  let read = false
   function addBook() {
     books = [...books, ""]
   }
@@ -12,11 +13,13 @@
 
   {#each books as book, index}
     <input type="text" bind:value={books[index]} />
+    <input type="checkbox" bind:checked={read} />
+    <p>The book is read: {read}</p>
   {/each}
 </main>
 
-<!-- Steps: Make addbook have details of title and author 
- Give the books a read/unread tick box beside each - might have to have a confirm for this so that it saves to local storage
+<!-- 
+ Give the books a read/unread tick box beside each - might have to have a confirm for this so that it saves to local storage yes and need to induvidualise the read state for each book as they are all the same when changed.
  Make all changes save to local storage and can reload-->
 
 <footer class="footer">
